40ea0d4626b80a14060aba59786d9f8c4447f9f3
JV13 <cherkas.ivan13@gmail.com>
2025-04-09 19:12:28 +0300

    check deploy ng-prime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { ButtonModule } from 'primeng/button';
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, ButtonModule],
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'jv13-craft-front';
+    title = 'jv13-craft-front';
 }
